6ffd920ab9bfa77287afacf120918a2dc0bb2bbe
欢 林 <643729712@qq.com>
2020-03-17 21:26:00 +0800

    111

--- PyMuta/src/cmodel/program.py
+++ PyMuta/src/cmodel/program.py
@@ -1,4 +1,5 @@
 import os
+import src.cmodel.cflow as cflow
 import src.cmodel.ccode as ccode
 import src.cmodel.mutant as cmutant
 
@@ -12,18 +13,22 @@ class Program:
         code_file = os.path.join(dir_path, self.name + '.c')
         ast_file = os.path.join(dir_path, self.name + '.ast')
         cir_file = os.path.join(dir_path, self.name + '.cir')
+        flw_file = os.path.join(dir_path, self.name + '.flw')
+        inf_file = os.path.join(dir_path, self.name + '.inf')
         mut_file = os.path.join(dir_path, self.name + '.mut')
         lab_file = os.path.join(dir_path, self.name + '.lab')
         sem_file = os.path.join(dir_path, self.name + '.sem')
         self.source_code = ccode.SourceCode(self, code_file)
         self.ast_tree = ccode.AstTree(self, ast_file)
         self.cir_tree = ccode.CirTree(self.ast_tree, cir_file)
+        self.function_graph = cflow.CirFunctionGraph(self, flw_file)
+        self.influence_graph = cflow.CInfluenceGraph(self, inf_file)
         self.mutant_space = cmutant.MutantSpace(self.ast_tree, self.cir_tree, mut_file, lab_file, sem_file)
         return
 
 
 def test_all():
-    data_directory = 'C:\\Users\\yukimula\\git\\jcsa\\PyMuta\\data'
+    data_directory = 'C:\\Users\\yukimula\\git\\jcsa\\JCMuta\\results\\data'
     for file_name in os.listdir(data_directory):
         file_directory = os.path.join(data_directory, file_name)
         program = Program(file_directory)
