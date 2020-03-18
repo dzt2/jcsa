f74ed79f9aecc53b5c0dbfbfdcbdce56da77f7ce
欢 林 <643729712@qq.com>
2020-03-18 18:10:53 +0800

    1234

---
+++ PyMuta/src/mining/classify.py
@@ -0,0 +1,213 @@
+import os
+import math
+import pydotplus
+from six import StringIO
+import src.cmodel.ccode as ccode
+import src.cmodel.mutant as cmutant
+import src.mining.encode as encode
+import sklearn.metrics as metrics
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn.tree as tree
+
+
+class MutantClassification:
+    """
+    Provide methods for determining the label of a set of mutants.
+    """
+
+    @staticmethod
+    def get_label_distribution(mutants):
+        """
+        :param mutants:
+        :return: {[0]: number; [1]: number}
+        """
+        distribution = dict()
+        distribution[0] = 0
+        distribution[1] = 0
+        for mutant in mutants:
+            label = mutant.labels.label
+            distribution[label] += 1
+        return distribution
+
+    @staticmethod
+    def get_main_label(mutants):
+        distribution = MutantClassification.get_label_distribution(mutants)
+        max_label, max_number = None, 0
+        for label, number in distribution.items():
+            if number > max_number:
+                max_label = label
+                max_number = number
+        return max_label
+
+    @staticmethod
+    def get_main_support(mutants):
+        distribution = MutantClassification.get_label_distribution(mutants)
+        max_label, max_number = None, 0
+        for label, number in distribution.items():
+            if number > max_number:
+                max_label = label
+                max_number = number
+        if max_label is None:
+            return 0
+        else:
+            return distribution[max_label]
+
+    @staticmethod
+    def get_support(mutants, target_label: int):
+        distribution = MutantClassification.get_label_distribution(mutants)
+        if target_label in distribution:
+            return distribution[target_label]
+        else:
+            return 0
+
+    @staticmethod
+    def get_main_confidence(mutants):
+        distribution = MutantClassification.get_label_distribution(mutants)
+        max_label, max_number = None, 0
+        for label, number in distribution.items():
+            if number > max_number:
+                max_label = label
+                max_number = number
+        if max_label is None or len(mutants) == 0:
+            return 0.0
+        else:
+            return max_number / (len(mutants) + 0.0)
+
+    @staticmethod
+    def get_confidence(mutants, target_label: int):
+        distribution = MutantClassification.get_label_distribution(mutants)
+        if target_label in distribution and len(mutants) > 0:
+            return distribution[target_label] / (len(mutants) + 0.0)
+        else:
+            return 0.0
+
+    @staticmethod
+    def get_entropy(mutants):
+        probability = MutantClassification.get_main_confidence(mutants)
+        if len(mutants) == 0 or probability == 1:
+            return 0.0
+        else:
+            return -(probability * math.log(probability + 1e-10, 2) + (1 - probability) *
+                     math.log(1 - probability + 1e-10, 2))
+
+
+class FeatureClassifierEvaluate:
+    """
+    Used to evaluate the performance of feature model for classifying mutant as killed or non-killed
+    """
+
+    def __init__(self):
+        self.mapping = dict()
+        return
+
+    @staticmethod
+    def __key__(mutant: cmutant.Mutant):
+        feature_vector = list()
+        for feature in mutant.feature_vector:
+            feature_vector.append(feature)
+        feature_vector.sort()
+        return mutant.space.program.name + "#" + str(feature_vector)
+
+    def fit(self, data_frame: encode.MutantDataFrame):
+        for mutant in data_frame.program.mutant_space.get_mutants():
+            mutant: cmutant.Mutant
+            key = FeatureClassifierEvaluate.__key__(mutant)
+            if key not in self.mapping:
+                self.mapping[key] = list()
+            self.mapping[key].append(mutant)
+        return
+
+    def __evaluate_feature__(self, mutant: cmutant.Mutant):
+        """
+        :param mutant:
+        :return: feature_string, feature_size, predict_label, main_support, main_confidence, feature_entropy
+        """
+        feature_string = FeatureClassifierEvaluate.__key__(mutant)
+        mutants = self.mapping[feature_string]
+        feature_size = len(mutant.feature_vector)
+        predict_label = MutantClassification.get_main_label(mutants)
+        main_support = MutantClassification.get_main_support(mutants)
+        main_confidence = MutantClassification.get_main_confidence(mutants)
+        feature_entropy = MutantClassification.get_entropy(mutants)
+        return [feature_string, feature_size, predict_label, main_support, main_confidence, feature_entropy]
+
+    @staticmethod
+    def __generate_line_of_mutant__(mutant: cmutant.Mutant, feature_line):
+        """
+        program, id, class, operator, line, code_segment, parameter?,
+        label, p_label, result, feature, feature_size, main_support, main_confidence, feature_entropy
+        :param feature_line:
+        :param mutant:
+        :return:
+        """
+        program = mutant.space.program.name
+        identifier = mutant.get_id()
+        m_class = mutant.muta_class
+        m_operator = mutant.muta_operator
+        ast_location = mutant.get_location()
+        source_code = mutant.space.program.source_code
+        source_code: ccode.SourceCode
+        line = source_code.line_of(ast_location.beg_index)
+        code_segment = '\"' + ast_location.get_code(True) + '\"'
+        parameter = ''
+        if mutant.has_parameter():
+            parameter = str(mutant.get_parameter())
+        label = mutant.labels.label
+        result = (label == feature_line[2])
+        return [program, identifier, m_class, m_operator, line, code_segment, parameter,
+                label, feature_line[2], result, mutant.feature_words, len(mutant.feature_vector),
+                feature_line[3], feature_line[4], feature_line[5]]
+
+    def evaluate(self, file_path: str):
+        entropy_list, labels, p_labels = list(), list(), list()
+        error_map = dict()
+        error_size = 0
+        with open(file_path, 'w') as writer:
+            writer.write('program\tid\tclass\toperator\tline\tlocation\tparameter'
+                         'label\tp_label\tresult\tfeature\tfeature_size\tsupport\tconfidence\tentropy\n')
+            for key, mutants in self.mapping.items():
+                feature_line = self.__evaluate_feature__(mutants[0])
+                entropy_list.append(feature_line[5])
+                for mutant in mutants:
+                    mutant: cmutant.Mutant
+                    labels.append(mutant.labels.label)
+                    p_labels.append(feature_line[2])
+                    str_line = FeatureClassifierEvaluate.__generate_line_of_mutant__(mutant, feature_line)
+                    for argument in str_line:
+                        writer.write(str(argument))
+                        writer.write('\t')
+                    writer.write('\n')
+                    if mutant.labels.label != feature_line[2]:
+                        m_class = mutant.get_muta_class()
+                        if m_class not in error_map:
+                            error_map[m_class] = 0
+                        error_map[m_class] += 1
+                        error_size += 1
+        print(metrics.classification_report(labels, p_labels, target_names=['Equiv', 'Trivial']))
+        print()
+        print('In total there are', error_size, 'errors in feature space.')
+        print(error_map)
+        sns.kdeplot(entropy_list, color='r', label='feature-entropy')
+        plt.show()
+        return
+
+
+def evaluate_classifier(get_assertions, assertion_encode, prob_threshold):
+    data_directory = 'C:\\Users\\yukimula\\git\\jcsa\\JCMuta\\results\\data'
+    output_directory = 'C:\\Users\\yukimula\\git\\jcsa\\PyMuta\\output'
+    evaluator = FeatureClassifierEvaluate()
+    for file_name in os.listdir(data_directory):
+        program_directory = os.path.join(data_directory, file_name)
+        encoder = encode.MutantFeatureEncoder(get_assertions, assertion_encode, prob_threshold)
+        data_frame = encode.MutantDataFrame(program_directory, encoder)
+        print('Load', len(data_frame.program.mutant_space.mutants), 'mutants from', file_name)
+        evaluator.fit(data_frame)
+    print()
+    evaluator.evaluate(os.path.join(output_directory, 'feature_evaluate.txt'))
+    return
+
+
+if __name__ == "__main__":
+    evaluate_classifier(encode.SemanticAssertionEncodeFunctions.get_all_error_assertions,
+                        encode.SemanticAssertionEncodeFunctions.get_assertion_string, 0.002)
