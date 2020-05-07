2651c2a474c966a2923f6c032973066ffbddcdd6
欢 林 <643729712@qq.com>
2020-05-07 17:49:36 +0800

    1234

---
+++ JCMuta/src/com/jcsa/jcmuta/mutant/error2mutation/infection/oexa/ASGBORInfection.java
@@ -0,0 +1,45 @@
+package com.jcsa.jcmuta.mutant.error2mutation.infection.oexa;
+
+import java.util.Map;
+
+import com.jcsa.jcmuta.mutant.error2mutation.StateError;
+import com.jcsa.jcmuta.mutant.error2mutation.StateErrorGraph;
+import com.jcsa.jcmuta.mutant.error2mutation.StateEvaluation;
+import com.jcsa.jcmuta.mutant.error2mutation.infection.OPRTInfection;
+import com.jcsa.jcparse.lang.irlang.expr.CirExpression;
+import com.jcsa.jcparse.lang.irlang.stmt.CirStatement;
+import com.jcsa.jcparse.lang.lexical.COperator;
+import com.jcsa.jcparse.lang.symb.StateConstraints;
+import com.jcsa.jcparse.lang.symb.SymExpression;
+
+public class ASGBORInfection extends OPRTInfection {
+
+	protected SymExpression muta_expression(CirExpression expression, CirExpression loperand, CirExpression roperand)
+			throws Exception {
+		return StateEvaluation.binary_expression(expression.get_data_type(), COperator.bit_or, loperand, roperand);
+	}
+
+	@Override
+	protected boolean partial_evaluate(CirExpression expression, CirExpression loperand, CirExpression roperand,
+			StateErrorGraph graph, Map<StateError, StateConstraints> output) throws Exception {
+		return false;
+	}
+
+	@Override
+	protected boolean symbolic_evaluate(CirExpression expression, CirExpression loperand, CirExpression roperand,
+			StateErrorGraph graph, Map<StateError, StateConstraints> output) throws Exception {
+		SymExpression lcondition, rcondition; StateConstraints constraints;
+		CirStatement statement = expression.statement_of();
+		
+		lcondition = StateEvaluation.binary_expression(expression.get_data_type(),
+				COperator.bit_and, loperand, roperand);
+		rcondition = StateEvaluation.not_equals(StateEvaluation.get_symbol(roperand), 
+				lcondition);
+		constraints = StateEvaluation.get_conjunctions();
+		this.add_constraint(constraints, statement, rcondition);
+		output.put(graph.get_error_set().chg_numb(expression), constraints);
+		
+		return true;
+	}
+
+}
