fd21775a918e76214ea1d0407ef08be6e88191bd
欢 林 <643729712@qq.com>
2020-05-07 17:25:23 +0800

    1234

---
+++ JCMuta/src/com/jcsa/jcmuta/mutant/error2mutation/infection/orbn/GREBANInfection.java
@@ -0,0 +1,96 @@
+package com.jcsa.jcmuta.mutant.error2mutation.infection.orbn;
+
+import java.util.Map;
+
+import com.jcsa.jcmuta.mutant.error2mutation.StateError;
+import com.jcsa.jcmuta.mutant.error2mutation.StateErrorGraph;
+import com.jcsa.jcmuta.mutant.error2mutation.StateEvaluation;
+import com.jcsa.jcmuta.mutant.error2mutation.infection.OPRTInfection;
+import com.jcsa.jcparse.lang.irlang.expr.CirExpression;
+import com.jcsa.jcparse.lang.irlang.stmt.CirStatement;
+import com.jcsa.jcparse.lang.lexical.COperator;
+import com.jcsa.jcparse.lang.symb.StateConstraints;
+import com.jcsa.jcparse.lang.symb.SymExpression;
+
+public class GREBANInfection extends OPRTInfection {
+
+	@Override
+	protected SymExpression muta_expression(CirExpression expression, CirExpression loperand, CirExpression roperand)
+			throws Exception {
+		return StateEvaluation.binary_expression(expression.
+				get_data_type(), COperator.bit_and, loperand, roperand);
+	}
+
+	@Override
+	protected boolean partial_evaluate(CirExpression expression, CirExpression loperand, CirExpression roperand,
+			StateErrorGraph graph, Map<StateError, StateConstraints> output) throws Exception {
+		Object lconstant = StateEvaluation.get_constant_value(loperand);
+		Object rconstant = StateEvaluation.get_constant_value(roperand);
+		SymExpression constraint; StateConstraints constraints;
+		CirStatement statement = expression.statement_of();
+		
+		if(!(lconstant instanceof SymExpression)) {
+			if(StateEvaluation.is_zero_number(lconstant)) {
+				constraint = StateEvaluation.greater_eq(loperand, roperand);
+				constraints = StateEvaluation.get_conjunctions();
+				this.add_constraint(constraints, statement, constraint);
+				output.put(graph.get_error_set().set_bool(expression, false), constraints);
+			}
+			else {
+				constraint = StateEvaluation.equal_with(roperand, 0L);
+				constraints = StateEvaluation.get_conjunctions();
+				this.add_constraint(constraints, statement, constraint);
+				output.put(graph.get_error_set().set_bool(expression, false), constraints);
+				
+				constraint = StateEvaluation.smaller_tn(loperand, roperand);
+				constraints = StateEvaluation.get_conjunctions();
+				this.add_constraint(constraints, statement, constraint);
+				output.put(graph.get_error_set().set_bool(expression, true), constraints);
+			}
+			return true;
+		}
+		
+		if(rconstant instanceof Long) {
+			if(StateEvaluation.is_zero_number(rconstant)) {
+				constraint = StateEvaluation.greater_eq(loperand, roperand);
+				constraints = StateEvaluation.get_conjunctions();
+				this.add_constraint(constraints, statement, constraint);
+				output.put(graph.get_error_set().set_bool(expression, false), constraints);
+			}
+			else {
+				constraint = StateEvaluation.equal_with(loperand, 0L);
+				constraints = StateEvaluation.get_conjunctions();
+				this.add_constraint(constraints, statement, constraint);
+				output.put(graph.get_error_set().set_bool(expression, false), constraints);
+				
+				constraint = StateEvaluation.smaller_tn(loperand, roperand);
+				constraints = StateEvaluation.get_conjunctions();
+				this.add_constraint(constraints, statement, constraint);
+				output.put(graph.get_error_set().set_bool(expression, true), constraints);
+			}
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	protected boolean symbolic_evaluate(CirExpression expression, CirExpression loperand, CirExpression roperand,
+			StateErrorGraph graph, Map<StateError, StateConstraints> output) throws Exception {
+		SymExpression constraint; StateConstraints constraints;
+		CirStatement statement = expression.statement_of();
+		
+		constraint = StateEvaluation.smaller_tn(loperand, roperand);
+		constraints = StateEvaluation.get_conjunctions();
+		this.add_constraint(constraints, statement, constraint);
+		output.put(graph.get_error_set().set_bool(expression, true), constraints);
+		
+		constraints = StateEvaluation.get_disjunctions();
+		this.add_constraint(constraints, statement, StateEvaluation.equal_with(loperand, 0L));
+		this.add_constraint(constraints, statement, StateEvaluation.equal_with(roperand, 0L));
+		output.put(graph.get_error_set().set_bool(expression, false), constraints);
+		
+		return true;
+	}
+
+}
