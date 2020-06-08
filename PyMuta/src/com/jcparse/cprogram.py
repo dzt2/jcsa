b1da16381eb5c2d5468a85daf179144a40447c8c
欢 林 <643729712@qq.com>
2020-06-09 00:26:39 +0800

    111

--- PyMuta/src/com/jcparse/cprogram.py
+++ PyMuta/src/com/jcparse/cprogram.py
@@ -119,125 +119,93 @@ class CDominanceGraph:
         return
 
 
-class CDependenceType(Enum):
-    true_depend = 0
-    false_depend = 1
-    call_depend = 2
-    exit_depend = 3
-    use_define_depend = 4
-    parameter_argument_depend = 5
-    wait_return_depend = 6
-    parent_child_depend = 7
-
-    def __str__(self):
-        return self.name
-
-    @staticmethod
-    def parse(text: str):
-        if text == "predicate_depend::True":
-            return CDependenceType.true_depend
-        elif text == "predicate_depend::False":
-            return CDependenceType.false_depend
-        elif text == "stmt_exit_depend":
-            return CDependenceType.exit_depend
-        elif text == "stmt_call_depend":
-            return CDependenceType.call_depend
-        elif text == "use_defin_depend":
-            return CDependenceType.use_define_depend
-        elif text == "param_arg_depend":
-            return CDependenceType.parameter_argument_depend
-        elif text == "wait_retr_depend":
-            return CDependenceType.wait_return_depend
-        else:
-            return None     # invalid case
-
-
-class CDependenceEdge:
+class CInformationFlowType(Enum):
     """
-    dependence relationship as {type, source, target}
+    To describe the type of information flow in C program.
     """
-    def __init__(self, dependence_type: CDependenceType, source, target):
-        self.dependence_type = dependence_type
-        self.source = source
-        self.target = target
+    ''' A. information by control dependence '''
+    execute_in_stmt = 0     # assign_statement | if_statement | case_statement | call_statement  --> leafs
+    execute_in_true = 1     # if_statement.condition | case_statement.condition --> statement*
+    execute_in_false = 2    # if_statement.condition | case_statement.condition --> statement*
+    execute_in_call = 3     # call_statement.callee --> statement*
+    execute_in_exit = 4     # wait_assign_statement --> statement*
+    execute_in = 12         # execution --> statement
+
+    ''' B. information by data dependence '''
+    operand = 5             # operand --> parent as defer_expr, addr_expr, cast_expr, abbreviate compute_expr
+    operand_ = 6            # operand_{k} --> compute_expr, initializer_body
+    lr_assign = 7           # assign_statement.rvalue --> assign_statement.lvalue
+    df_assign = 8           # definition --> usage_point*
+    argument_ = 9           # expression_{k} --> parent as argument_list
+    arguments = 10          # argument_list --> callee in call_function
+    apply_callee = 11       # callee in call_statement --> wait_expr in wait_assign_statement
+
+
+class CInformationFlow:
+    """
+    {flow_type, parameter}
+    """
+    def __init__(self, flow_type: CInformationFlowType, parameter: int):
+        self.flow_type = flow_type
+        self.parameter = parameter
         return
 
-    def get_dependence_type(self):
-        return self.dependence_type
+    def get_flow_type(self):
+        return self.flow_type
+
+    def get_parameter(self):
+        return self.parameter
+
+    def __str__(self):
+        string = str(self.flow_type)
+        if string.endswith('_'):
+            return string + str(self.parameter)
+        else:
+            return string
+
+
+class CInformationEdge:
+    """
+    {flow, source, target}
+    """
+    def __init__(self, source, target, flow: CInformationFlow):
+        self.source = source
+        self.target = target
+        self.flow = flow
+        return
 
     def get_source(self):
         """
-        :return: node that depends on another via this relationship
+        :return: source node from the edge points
         """
         return self.source
 
     def get_target(self):
         """
-        :return: node that is depended by another via this relationship
+        :return: target node to the edge pointed
         """
         return self.target
 
-    def __str__(self):
-        return str(self.dependence_type) + "::(" + str(self.source) + ", " + str(self.target) + ")"
-
-    def is_control_dependence(self):
-        return self.dependence_type == CDependenceType.true_depend or \
-               self.dependence_type == CDependenceType.false_depend or \
-               self.dependence_type == CDependenceType.call_depend or \
-               self.dependence_type == CDependenceType.exit_depend
-
-    def is_data_dependence(self):
-        return self.dependence_type == CDependenceType.use_define_depend or \
-               self.dependence_type == CDependenceType.parameter_argument_depend or \
-               self.dependence_type == CDependenceType.wait_return_depend
-
-    def is_operand_dependence(self):
-        return self.dependence_type == CDependenceType.parent_child_depend
+    def get_flow(self):
+        """
+        :return: information flow that defines the edge
+        """
+        return self.flow
 
 
-class CDependenceNodeType(Enum):
-    execution = 0
-    condition = 1
-    reference = 2
-    expression = 3
-
-
-class CDependenceNode:
+class CInformationNode:
     """
-    node in dependence graph can be:
-        (1) execution --> condition {true_depend or false_depend}
-        (2) execution --> execution {call_depend or exit_depend}
-        (3) reference --> reference {use_define_depend}
-        (4) reference --> expression{parameter_argument_depend}
-        (5) wait_expr --> ret_point {wait_return_depend}
+    {graph, instance, in_edges, ou_edges}
     """
     def __init__(self, graph, instance: cirinst.CirInstance):
         self.graph = graph
         self.instance = instance
         self.in_edges = list()
         self.ou_edges = list()
-        self.node_type = None
-        if isinstance(instance, cirinst.CirInstanceNode):
-            self.node_type = CDependenceNodeType.execution
-        elif isinstance(instance, cirinst.CirInstanceCode):
-            instance: cirinst.CirInstanceCode
-            cir_node = instance.get_cir_source_node()
-            if cir_node.is_expression():
-                if cir_node.parent.get_cir_type() == cirtree.CirType.if_statement or \
-                        cir_node.parent.get_cir_type() == cirtree.CirType.case_statement:
-                    self.node_type = CDependenceNodeType.condition
-                elif cir_node.is_reference_expression():
-                    self.node_type = CDependenceNodeType.reference
-                else:
-                    self.node_type = CDependenceNodeType.expression
         return
 
     def get_graph(self):
         return self.graph
-
-    def get_node_type(self):
-        self.node_type: CDependenceNodeType
-        return self.node_type
 
     def get_instance(self):
         return self.instance
@@ -248,138 +216,68 @@ class CDependenceNode:
     def get_ou_edges(self):
         return self.ou_edges
 
-    def depend_on(self, target, dependence_type: CDependenceType):
-        target: CDependenceNode
-        edge = CDependenceEdge(dependence_type, self, target)
+    def link_to(self, target, flow_type: CInformationFlowType, parameter=0):
+        """
+        :param target:
+        :param flow_type:
+        :param parameter:
+        :return: information flow edge in graph
+        """
+        flow = CInformationFlow(flow_type, parameter)
+        edge = CInformationEdge(self, target, flow)
         self.ou_edges.append(edge)
         target.in_edges.append(edge)
         return edge
 
-    def __str__(self):
-        if isinstance(self.instance, cirinst.CirInstanceNode):
-            return str(self.instance)
-        else:
-            self.instance: cirinst.CirInstanceCode
-            return "[" + str(self.instance.context) + ", " + \
-                   str(self.instance.get_instance_node().get_source_execution()) + "::\"" + \
-                   self.instance.get_cir_source_node().generate_code(True) + "\"]"
 
-
-class CDependenceGraph:
-    def __init__(self, instance_graph: cirinst.CirInstanceGraph, dependence_file: str):
-        self.program = None
-        self.instance_graph = instance_graph
+class CInformationFlowGraph:
+    """
+    information flow graph as {program, nodes}
+    """
+    def __init__(self, program, dep_file: str):
+        self.program = program
         self.nodes = dict()
-        self.__parse__(dependence_file)
+        self.__create_nodes__()
         return
-
-    def get_instance_graph(self):
-        return self.instance_graph
-
-    def get_instances(self):
-        """
-        :return: set of instances w.r.t. nodes in the graph
-        """
-        return self.nodes.keys()
 
     def get_nodes(self):
         return self.nodes.values()
 
-    def get_node(self, instance: cirinst.CirInstance):
-        node = self.nodes[instance]
-        node: CDependenceNode
-        return node
+    def get_instances(self):
+        return self.nodes.keys()
 
-    def __elements_of__(self, text: str):
-        """
-        :param text:
-        :return: [predicate|reference, cir_node, bool|cir_node]
-        """
-        elements = list()
-        items = text.strip().split(' ')
-        self.instance_graph: cirinst.CirInstanceGraph
-        cir_tree = self.instance_graph.get_cir_tree()
-        name = items[1].strip()
-        if name == "predicate":
-            elements.append(name)
-            elements.append(cir_tree.get_node(base.get_content_of(items[2].strip())))
-            elements.append(base.get_content_of(items[3].strip()))
-        else:
-            elements.append(name)
-            elements.append(cir_tree.get_node(base.get_content_of(items[2].strip())))
-            elements.append(cir_tree.get_node(base.get_content_of(items[3].strip())))
-        return elements
+    def get_program(self):
+        self.program: CProgram
+        return self.program
 
-    def __new_node__(self, instance: cirinst.CirInstance):
-        """
-        :param instance:
-        :return: a newly created node or existing one w.r.t. the instance in graph
-        """
-        if instance is None:
-            return None
-        else:
-            if instance not in self.nodes:
-                node = CDependenceNode(self, instance)
-                self.nodes[instance] = node
-            return self.nodes[instance]
-
-    def __build_nodes_in__(self, parent: CDependenceNode, instance_node: cirinst.CirInstanceNode,
-                           cir_node: cirtree.CirNode):
-        child = self.__new_node__(instance_node.get_cir_instance_of(cir_node))
-        child: CDependenceNode
-        parent.depend_on(child, CDependenceType.parent_child_depend)
-        for cir_child in cir_node.get_children():
-            self.__build_nodes_in__(child, instance_node, cir_child)
-        return
-
-    def __build_nodes__(self, instance_node: cirinst.CirInstanceNode):
-        parent = self.__new_node__(instance_node)
-        self.__build_nodes_in__(parent, instance_node, instance_node.get_source_statement())
-        return
-
-    def __parse__(self, dependence_file: str):
+    def __create_nodes__(self):
         self.nodes.clear()
-        ''' create nodes and child-parent dependence edges '''
-        with open(dependence_file, 'r') as reader:
-            for line in reader:
-                line = line.strip()
-                if len(line) > 0:
-                    items = line.split('\t')
-                    if items[0].strip() == "[node]":
-                        instance_node = self.instance_graph.get_node(base.get_content_of(items[1].strip()))
-                        instance_node: cirinst.CirInstanceNode
-                        self.__build_nodes__(instance_node)
-        ''' create edges between statements instances '''
-        with open(dependence_file, 'r') as reader:
-            for line in reader:
-                line = line.strip()
-                if len(line) > 0:
-                    items = line.split('\t')
-                    if items[0].strip() == "[edge]":
-                        source_instance = self.instance_graph.get_node(base.get_content_of(items[1].strip()))
-                        target_instance = self.instance_graph.get_node(base.get_content_of(items[2].strip()))
-                        source_instance: cirinst.CirInstanceNode
-                        target_instance: cirinst.CirInstanceNode
-                        if len(items) < 5:
-                            dependence_type = CDependenceType.parse(items[3].strip())
-                        else:
-                            elements = self.__elements_of__(items[4].strip())
-                            if elements[0] == "predicate":
-                                target_instance = target_instance.get_cir_instance_of(elements[1])
-                                if elements[2]:
-                                    dependence_type = CDependenceType.true_depend
-                                else:
-                                    dependence_type = CDependenceType.false_depend
-                            else:
-                                source_instance = source_instance.get_cir_instance_of(elements[2])
-                                target_instance = target_instance.get_cir_instance_of(elements[1])
-                                dependence_type = CDependenceType.parse(items[3].strip())
-                        source = self.nodes[source_instance]
-                        target = self.nodes[target_instance]
-                        source: CDependenceNode
-                        target: CDependenceNode
-                        source.depend_on(target, dependence_type)
+        instance_graph = program.get_instance_graph()
+        for instance_node in instance_graph.get_nodes():
+            instance_node: cirinst.CirInstanceNode
+            self.nodes[instance_node] = CInformationNode(self, instance_node)
+            for code_instance in instance_node.get_code_instances():
+                self.nodes[code_instance] = CInformationNode(self, code_instance)
         return
+
+    def __create_non_dependency__(self, instance: cirinst.CirInstance):
+        """
+        :return:
+        """
+        if isinstance(instance, cirinst.CirInstanceNode):
+            instance: cirinst.CirInstanceNode
+            if not instance.get_source_statement().is_tag_statement():
+                source = self.nodes[instance]
+                target = self.nodes[instance.get_root_code_instance()]
+                source: CInformationNode
+                target: CInformationNode
+                source.link_to(target, CInformationFlowType.execute_in)
+        else:
+            instance: cirinst.CirInstanceCode
+            cir_source_node = instance.get_cir_source_node()
+            # TODO implement non-dependence information flow
+        return
+
 
 
 class CProgram:
@@ -414,7 +312,6 @@ class CProgram:
                                                     os.path.join(directory_path, filename + ".pre"))
         self.post_dominance_graph = CDominanceGraph(self.instance_graph,
                                                     os.path.join(directory_path, filename + ".pos"))
-        self.dependence_graph = CDependenceGraph(self.instance_graph, os.path.join(directory_path, filename + ".dep"))
         self.source_code.program = self
         self.ast_tree.program = self
         self.cir_tree.program = self
@@ -422,7 +319,6 @@ class CProgram:
         self.instance_graph.program = self
         self.prev_dominance_graph.program = self
         self.post_dominance_graph.program = self
-        self.dependence_graph.program = self
         return
 
     def get_directory(self):
@@ -452,9 +348,6 @@ class CProgram:
     def get_post_dominance_graph(self):
         return self.post_dominance_graph
 
-    def get_dependence_graph(self):
-        return self.dependence_graph
-
 
 if __name__ == "__main__":
     prefix = "C:\\Users\\yukimula\\git\\jcsa\\JCMuta\\results\\data"
@@ -463,7 +356,6 @@ if __name__ == "__main__":
         program = CProgram(directory)
         function_call_graph = program.get_function_call_graph()
         instance_graph = program.get_instance_graph()
-        dependence_graph = program.get_dependence_graph()
         output_file = os.path.join("C:\\Users\\yukimula\\git\\jcsa\\PyMuta\\output", filename + ".ast")
         print("Open the abstract syntax tree and CIR-tree for", filename)
         with open(output_file, 'w') as writer:
@@ -481,16 +373,5 @@ if __name__ == "__main__":
                             # write information about the node and its output edges
                             writer.write("\t" + str(instance_node.get_source_execution()) + "\t\"" +
                                          instance_node.get_source_statement().generate_code(True) + "\"\n")
-                            root = dependence_graph.get_node(instance_node)
-                            writer.write("\t\t" + str(root) + "\n")
-                            for edge in root.get_ou_edges():
-                                edge: CDependenceEdge
-                                writer.write("\t\t==>\t" + str(edge) + "\n")
-                            for instance_code in instance_node.get_cir_instances_in():
-                                node = dependence_graph.get_node(instance_code)
-                                writer.write("\t\t" + str(node) + "\n")
-                                for edge in node.get_ou_edges():
-                                    edge: CDependenceEdge
-                                    writer.write("\t\t==>\t" + str(edge) + "\n")
                     writer.write("End Function\n\n")
     print("Testing end for all...")
