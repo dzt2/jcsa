d16d09449e2c9f7474071b8178aa92b04cd51707
欢 林 <643729712@qq.com>
2020-06-30 23:29:15 +0800

    2345

--- JCParse/src/com/jcsa/jcparse/lang/sym/SymConstant.java
+++ JCParse/src/com/jcsa/jcparse/lang/sym/SymConstant.java
@@ -27,8 +27,18 @@ public class SymConstant extends SymBasicExpression {
 		case c_bool: 		return constant.get_bool();
 		case c_char:
 		case c_uchar:		return constant.get_char();
-		// TODO implement the symbolic modeling
-		default: return null;
+		case c_short:
+		case c_ushort:
+		case c_int:
+		case c_uint:		return constant.get_integer();
+		case c_long:
+		case c_ulong:
+		case c_llong:
+		case c_ullong:		return constant.get_long();
+		case c_float:		return constant.get_float();
+		case c_double:	
+		case c_ldouble:		return constant.get_double();
+		default: 			return null;
 		}
 	}
 
@@ -39,8 +49,38 @@ public class SymConstant extends SymBasicExpression {
 
 	@Override
 	protected String generate_code(boolean ast_code) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Object value = this.get_constant_value();
+		if(value instanceof Boolean) {
+			if(ast_code) {
+				if(((Boolean) value).booleanValue()) {
+					return "1";
+				}
+				else {
+					return "0";
+				}
+			}
+			else {
+				return value.toString();
+			}
+		}
+		else if(value instanceof Character) {
+			return Integer.valueOf(((Character) value).charValue()).toString();
+		}
+		else if(value instanceof Integer) {
+			return value.toString();
+		}
+		else if(value instanceof Long) {
+			return value.toString();
+		}
+		else if(value instanceof Float) {
+			return value.toString();
+		}
+		else if(value instanceof Double) {
+			return value.toString();
+		}
+		else {
+			throw new IllegalArgumentException("Invalid code: " + this.get_data_type());
+		}
 	}
 
 }
