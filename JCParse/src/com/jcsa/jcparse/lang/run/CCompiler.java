b1ce2909ff221d8875e6e95524da824173294655
dzt2 <dzt2@ubuntu>
2020-07-24 19:58:00 +0800

    compilation-impl

--- JCParse/src/com/jcsa/jcparse/lang/run/CCompiler.java
+++ JCParse/src/com/jcsa/jcparse/lang/run/CCompiler.java
@@ -18,44 +18,130 @@ public class CCompiler {
 	
 	/**
 	 * @param cfile the source code file before being preprecessed.
-	 * @param ifile the code file with preprocessed (without #include...)
+	 * @param tfile the code file with preprocessed (without #include...)
 	 * @param hfile the config/linux.h for removing special keyword in C89.
+	 * @param ifiles the directory where the .h files are included.
 	 * @return the command for running pre-processing compilation.
 	 * @throws Exception
 	 */
-	public String preprocess_command(File cfile, File ifile, File hfile) throws Exception {
-		String command = String.format(preprocess_template, 
+	public String preprocess_command(File cfile, File tfile, File hfile, Iterable<File> ifiles) throws Exception {
+		StringBuilder command = new StringBuilder();
+		command.append(String.format(preprocess_template, 
 				this.compiler.toString(), hfile.getAbsolutePath(), 
-				cfile.getAbsolutePath(), ifile.getAbsolutePath());
-		return command;
+				cfile.getAbsolutePath(), tfile.getAbsolutePath()));
+		for(File ifile : ifiles) { command.append(" -I ").append(ifile.getAbsolutePath()); }
+		return command.toString();
 	}
 	
 	/**
 	 * perform preprocess command on specified inputs.
 	 * @param cfile the source code file before being preprecessed.
-	 * @param ifile the code file with preprocessed (without #include...)
+	 * @param tfile the code file with preprocessed (without #include...)
 	 * @param hfile the config/linux.h for removing special keyword in C89.
+	 * @param ifiles the directory where the .h files are included.
 	 * @return true if the pre-processing-compilation success.
 	 * @throws Exception
 	 */
-	public boolean do_preprocess(File cfile, File ifile, File hfile) throws Exception {
+	public boolean do_preprocess(File cfile, File tfile, File hfile, Iterable<File> ifiles) throws Exception {
 		/* 1. generate the command for running preprocessing */
-		String command = this.preprocess_command(cfile, ifile, hfile);
+		String command = this.preprocess_command(cfile, tfile, hfile, ifiles);
 		String[] commands = command.strip().split(" ");
 		
 		/* 2. delete the original intermediate file for being over-written */
-		if(ifile.exists()) {
-			ifile.delete();
-			while(ifile.exists());
+		if(tfile.exists()) {
+			tfile.delete();
+			while(!tfile.exists());
 		}
 		
 		/* 3. perform command processing for pre-processing source file */
 		CommandProcess.do_process(commands, null, CommandProcess.buffer_size_0);
 		
 		/* 4. return true if the generation succeeds. */
-		return ifile.exists();
+		return tfile.exists();
 	}
 	
+	/**
+	 * clang|gcc 
+	 * @param cfiles the set of .c files being compiled together
+	 * @param hfiles the directories where .h files are included.
+	 * @param lfiles the directories where library files are linked.
+	 * @param params other parameters used in compilation.
+	 * @param exe_file the executional file being compiled.
+	 * @return the command for running compilation
+	 * @throws Exception
+	 */
+	public String compile_command(Iterable<File> cfiles, 
+			Iterable<File> hfiles, 
+			Iterable<File> lfiles,
+			Iterable<String> params,
+			File exe_file) throws Exception {
+		StringBuilder command = new StringBuilder();
+		
+		command.append(this.compiler.toString());
+		for(File cfile : cfiles) {
+			command.append(" ").append(cfile.getAbsolutePath());
+		}
+		for(File hfile : hfiles) {
+			command.append(" -I ").append(hfile.getAbsolutePath());
+		}
+		for(File lfile : lfiles) {
+			command.append(" -L ").append(lfile.getAbsolutePath());
+		}
+		for(String param : params) {
+			command.append(" ").append(param.strip());
+		}
+		command.append(" -o ").append(exe_file.getAbsolutePath());
+		
+		return command.toString();
+	}
 	
+	/**
+	 * @param cfiles the set of .c files being compiled together
+	 * @param hfiles the directories where .h files are included.
+	 * @param lfiles the directories where library files are linked.
+	 * @param params other parameters used in compilation.
+	 * @param exe_file the executional file being compiled.
+	 * @return true if the executional file is generated.
+	 * @throws Exception
+	 */
+	public boolean do_compile(Iterable<File> cfiles, 
+			Iterable<File> hfiles, 
+			Iterable<File> lfiles,
+			Iterable<String> params,
+			File exe_file) throws Exception {
+		/* 1. generate the command for running compilation */
+		String command = this.compile_command(cfiles, hfiles, lfiles, params, exe_file);
+		String[] commands = command.strip().split(" ");
+		
+		/* 2. remove the original executional file */
+		if(exe_file.exists()) {
+			exe_file.delete();
+			while(!exe_file.exists());
+		}
+		
+		/* 3. perform command processing for compiling source file */
+		CommandProcess.do_process(commands, null, CommandProcess.buffer_size_1);
+		
+		/* 4. return whether exe file is generated */	return exe_file.exists();
+	}
+	
+	/**
+	 * @param compiler the program used in compilation
+	 */
+	private CCompiler(CCompilers compiler) throws Exception {
+		if(compiler == null)
+			throw new IllegalArgumentException("Invalid compiler: null");
+		this.compiler = compiler;
+	}
+	
+	/**
+	 * create a compiler with specified program being used.
+	 * @param compiler
+	 * @return
+	 * @throws Exception
+	 */
+	public static CCompiler get_compiler(CCompilers compiler) throws Exception {
+		return new CCompiler(compiler);
+	}
 	
 }
---
+++ JCParse/src/test/CompilationTest.java
@@ -0,0 +1,63 @@
+package test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jcsa.jcparse.lang.run.CCompiler;
+import com.jcsa.jcparse.lang.run.CCompilers;
+
+public class CompilationTest {
+	
+	private static final String cdirectory = "/home/dzt2/Development/DataSet/Code/cfiles/";
+	private static final String idirectory = "/home/dzt2/Development/DataSet/Code/sfiles/";
+	private static final String edirectory = "/home/dzt2/Development/DataSet/Code/efiles/";
+	
+	public static void main(String[] args) throws Exception {
+		File[] cfiles = new File(cdirectory).listFiles();
+		for(File cfile : cfiles) {
+			if(cfile.getName().endsWith(".c")) {
+				System.out.println("Testing on " + cfile.getName());
+				File tfile = testing_preprocess(cfile);
+				testing_compilation(tfile);
+				System.out.println();
+			}
+		}
+	}
+	
+	protected static File testing_preprocess(File source_file) throws Exception {
+		File tfile = new File(idirectory + source_file.getName());
+		File hfile = new File("config/linux.h");
+		List<File> hfiles = new ArrayList<File>();
+		hfiles.add(new File(cdirectory));
+		
+		CCompiler compiler = CCompiler.get_compiler(CCompilers.clang);
+		if(compiler.do_preprocess(source_file, tfile, hfile, hfiles)) {
+			System.out.println("\t1. Preprocess to generate " + tfile.getAbsolutePath());
+			return tfile;
+		}
+		else {
+			throw new RuntimeException("Unable to preprocess " + source_file.getAbsolutePath());
+		}
+	}
+	
+	protected static File testing_compilation(File tfile) throws Exception {
+		List<File> cfiles = new ArrayList<File>();
+		cfiles.add(tfile);
+		List<File> hfiles = new ArrayList<File>();
+		List<File> lfiles = new ArrayList<File>();
+		List<String> params = new ArrayList<String>();
+		params.add("-lm");
+		File efile = new File(edirectory + tfile.getName() + ".exe");
+		
+		CCompiler compiler = CCompiler.get_compiler(CCompilers.clang);
+		if(compiler.do_compile(cfiles, hfiles, lfiles, params, efile)) {
+			System.out.println("\t2. Succeed to compile " + efile.getAbsolutePath());
+		}
+		else {
+			System.out.println("\t2. Failed to compile " + efile.getAbsolutePath());
+		}
+		return efile;
+	}
+	
+}
