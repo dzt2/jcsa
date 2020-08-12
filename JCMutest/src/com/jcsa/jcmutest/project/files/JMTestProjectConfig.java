f89ce27f2dc38e0d57618acd0c5a4d9b9a4472e7
dzt2 <dzt2@ubuntu>
2020-08-12 22:16:20 +0800

    123

---
+++ JCMutest/src/com/jcsa/jcmutest/project/files/JMTestProjectConfig.java
@@ -0,0 +1,15 @@
+package com.jcsa.jcmutest.project.files;
+
+/**
+ * c_template_file	[config/cruntime.txt]
+ * c_macros_file	[config/linux.h]
+ * jcmutest.h		[config/jcmutest.h]
+ * jcinst.h			[config/jcinst.h]
+ * parameters		{ compiler.name, c_compiler_parameters, language_standard }
+ * 
+ * @author yukimula
+ *
+ */
+public class JMTestProjectConfig {
+
+}
