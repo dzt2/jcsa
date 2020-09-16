96ca070abab765707da453d34d15ada344cb9de7
dzt2 <dzt2@Ubuntu-64>
2020-09-16 21:03:25 +0800

    111

--- JCMutest/src/test/MuTestProjectExecute.java
+++ JCMutest/src/test/MuTestProjectExecute.java
@@ -29,7 +29,7 @@ public class MuTestProjectExecute {
 	private static final long max_timeout_seconds = 1;
 	
 	public static void main(String[] args) throws Exception {
-		testing("bi_search.c", 0, 0);
+		testing("profit.c", 0, 0);
 	}
 	
 	private static String get_name(File cfile) {
