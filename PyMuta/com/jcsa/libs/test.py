4500a76cf37dfa98f0b3aa888695a9a0f0accdc6
dzt2 <dzt2@ubuntu>
2021-05-08 17:35:37 +0800

    2

--- PyMuta/com/jcsa/libs/test.py
+++ PyMuta/com/jcsa/libs/test.py
@@ -1,7 +1,5 @@
 """
-This file develops the data model for describing the execution information during test execution, including:
-	---	xxx.cov: the coverage matrix which records of which statement is covered by which test case.
-	---	xxx.sit: the symbolic instance paths annotated with symbolic conditions for analysis and mining.
+This file defines the data model of feature and coverage information.
 """
 
 
@@ -19,12 +17,20 @@ class CDocument:
 	def __init__(self, directory: str, name: str):
 		self.project = jcmuta.CProject(directory, name)
 		cov_file_path = os.path.join(directory, name + ".cov")
-		sit_file_path = os.path.join(directory, name + ".sit")
+		sip_file_path = os.path.join(directory, name + ".sip")
 		self.coverage_matrix = CoverageMatrix(self, cov_file_path)
 		self.conditions = SymConditions(self)
-		self.sequences = list()
-		self.mutant_sequences = dict()
-		self.__loading__(sit_file_path)
+		self.paths = list()
+		self.mutant_paths = dict()
+		with open(sip_file_path, 'r') as reader:
+			for line in reader:
+				line = line.strip()
+				if len(line) > 0:
+					sym_instance_path = SymInstancePath(self, line)
+					self.paths.append(sym_instance_path)
+					if not (sym_instance_path.get_mutant() in self.mutant_paths):
+						self.mutant_paths[sym_instance_path.get_mutant()] = list()
+					self.mutant_paths[sym_instance_path.get_mutant()].append(sym_instance_path)
 		return
 
 	def get_project(self):
@@ -39,97 +45,28 @@ class CDocument:
 	def get_conditions_lib(self):
 		return self.conditions
 
-	def get_sequences(self):
+	def get_paths(self):
 		"""
-		:return: the collection of sequence of execution nodes annotated with symbolic conditions
+		:return: the collection of paths for killing a mutation using symbolic instances and conditions
 		"""
-		return self.sequences
+		return self.paths
 
 	def get_mutants(self):
-		return self.mutant_sequences.keys()
+		"""
+		:return: the set of mutations being killed by any paths under the document
+		"""
+		return self.mutant_paths.keys()
 
-	def get_sequences_of(self, target_mutant: jcmuta.Mutant):
+	def get_paths_of(self, target_mutant: jcmuta.Mutant):
 		"""
 		:param target_mutant:
 		:return: the set of symbolic sequence of execution nodes w.r.t. the mutant
 		"""
-		if target_mutant in self.mutant_sequences:
-			return self.mutant_sequences[target_mutant]
+		if target_mutant in self.mutant_paths:
+			return self.mutant_paths[target_mutant]
 		else:
-			return set()
+			return list()
 
-	def __wording__(self, word: str):
-		"""
-		:param word:
-		:return: generate the condition w.r.t. the word
-		"""
-		word = word.strip()
-		if len(word) > 0:
-			self.conditions.get_condition(word)
-		return word
-
-	def __produce__(self, seq, head: str, words):
-		"""
-		:param seq:
-		:param words: {stage$exec$result {condition_word}+ ;}
-		:return: SymExecution
-		"""
-		items = head.strip().split('$')
-		stage = jcbase.CToken.parse(items[0].strip()).get_token_value()
-		exec_tok = jcbase.CToken.parse(items[1].strip()).get_token_value()
-		result = jcbase.CToken.parse(items[2].strip()).get_token_value()
-		execution = self.project.program.function_call_graph.get_execution(exec_tok[0], exec_tok[1])
-		condition_words = list()
-		for word in words:
-			word = self.__wording__(word)
-			if len(word) > 0:
-				condition_words.append(word)
-		return SymExecution(seq, stage, execution, result, condition_words)
-
-	def __consume__(self, line: str):
-		"""
-		:param line: mid {head condition+ ;}+
-		:return:
-		"""
-		line = line.strip()
-		if len(line) > 0:
-			items = line.split('\t')
-			mutation = self.project.muta_space.get_mutant(int(items[0].strip()))
-			seq = SymSequence(self, mutation)
-			exec_list = list()
-			for i in range(1, len(items)):
-				item = items[i].strip()
-				if len(item) > 0:
-					if item != ';':
-						exec_list.append(item)
-					else:
-						head = exec_list[0]
-						words = exec_list[1:]
-						execution = self.__produce__(seq, head, words)
-						for word in execution.get_words():
-							if not(word in seq.get_words()):
-								seq.words.append(word)
-						seq.executions.append(execution)
-						exec_list.clear()
-			return seq
-		return None
-
-	def __loading__(self, file_path: str):
-		"""
-		:param file_path:
-		:return:
-		"""
-		with open(file_path, 'r') as reader:
-			for line in reader:
-				seq = self.__consume__(line.strip())
-				if not(seq is None):
-					seq: SymSequence
-					self.sequences.append(seq)
-					mutation = seq.get_mutant()
-					if not(mutation in self.mutant_sequences):
-						self.mutant_sequences[mutation] = set()
-					self.mutant_sequences[mutation].add(seq)
-		return
 
 
 class CoverageMatrix:
@@ -220,16 +157,11 @@ class CoverageMatrix:
 
 class SymCondition:
 	"""
-	It describes a symbolic condition annotated at some program point for killing a target mutation.
-		---	category: 	either "satisfaction" for constraint or "observations" for state error.
-		---	operator: 	the type of the symbolic condition being analyzed.
-		---	execution:	the execution point in control flow graph where the condition is checked.
-		---	location:	the C-intermediate code location where the condition is defined upon.
-		---	parameter:	the symbolic expression to refine the condition or None if not needed.
+	The symbolic condition defined in program context for killing a mutation.
 	"""
 
-	def __init__(self, category: str, operator: str, execution: jccode.CirExecution, location: jccode.CirNode,
-				 parameter: jcbase.SymNode):
+	def __init__(self, category: str, operator: str, execution: jccode.CirExecution,
+				 location: jccode.CirNode, parameter: jcbase.SymNode):
 		"""
 		:param category:	either "satisfaction" for constraint or "observations" for state error.
 		:param operator:	the type of the symbolic condition being analyzed.
@@ -351,145 +283,165 @@ class SymConditions:
 		return conditions
 
 
-class SymExecution:
+class SymInstance:
 	"""
-	It represents an execution point annotated with a series of symbolic conditions being met in an execution sequence.
+	The instance of a constraint or state error being evaluated in a testing path.
 	"""
 
-	def __init__(self, seq, stage: bool, execution: jccode.CirExecution, result: bool, words):
+	def __init__(self, path, execution: jccode.CirExecution, executed: bool, accepted: bool, rejected: bool):
 		"""
-		:param seq: 		the sequence of symbolic executions where the node is preserved
-		:param stage: 		True  ---	if the execution is performed before reaching the mutated point
-							False ---	if the execution is performed after the mutated point is reached
-		:param execution:	the execution point in control flow graph in form of C-intermediate representation
-		:param result:		True  ---	if the conditions among the node must be satisfied by any tests
-							False ---	if the conditions among the node be non-satisfiable by any test
-							None  ---	if the satisfaction of the conditions among depends on the test
-		:param words:		the collection of words encoding the symbolic conditions annotated with this node
+		:param path: the path where the instance is created
+		:param execution: where the instance is evaluated
+		:param executed: whether the instance is executed yet
+		:param accepted: whether the instance is accepted (evaluated as true)
+		:param rejected: whether the instance is always rejected (as false)
 		"""
-		seq: SymSequence
-		self.sequence = seq
-		self.stage = stage
+		path: SymInstancePath
+		self.path = path
 		self.execution = execution
-		self.result = result
-		self.words = list()
-		for word in words:
-			word: str
-			self.words.append(word.strip())
+		self.executed = executed
+		self.accepted = accepted
+		self.rejected = rejected
+		self.conditions = list()
 		return
 
-	def get_sequence(self):
+	def get_path(self):
 		"""
-		:return: the sequence of symbolic executions where the node is preserved
+		:return: the path where the instance is created
 		"""
-		return self.sequence
-
-	def get_stage(self):
-		"""
-		:return: 	True  ---	if the execution is performed before reaching the mutated point
-					False ---	if the execution is performed after the mutated point is reached
-		"""
-		return self.stage
+		return self.path
 
 	def get_execution(self):
 		"""
-		:return: 	the execution point in control flow graph in form of C-intermediate representation
+		:return: where the instance is evaluated
 		"""
 		return self.execution
 
-	def get_result(self):
+	def is_executed(self):
 		"""
-		:return:	True  ---	if the conditions among the node must be satisfied by any tests
-					False ---	if the conditions among the node be non-satisfiable by any test
-					None  ---	if the satisfaction of the conditions among depends on the test
+		:return: whether the instance is executed yet
 		"""
-		return self.result
+		return self.executed
 
-	def get_words(self):
+	def is_accepted(self):
 		"""
-		:return: the collection of words encoding the symbolic conditions annotated with this node
+		:return: whether the instance is accepted (evaluated as true)
 		"""
-		return self.words
+		return self.accepted
+
+	def is_rejected(self):
+		"""
+		:return: whether the instance is always rejected (as false)
+		"""
+		return self.rejected
+
+	def is_acceptable(self):
+		"""
+		:return: whether the instance can be accepted
+		"""
+		if self.executed:
+			return not self.rejected
+		else:
+			return False
+
+	def has_conditions(self):
+		"""
+		:return: whether any condition is used to define the instance
+		"""
+		return len(self.conditions) > 0
 
 	def get_conditions(self):
 		"""
-		:return: the collection of the symbolic conditions annotated with this node
+		:return: the set of conditions required within the instance evaluation
 		"""
-		document = self.sequence.document
-		return document.get_conditions_lib().get_conditions(self.words)
+		return self.conditions
 
 
-class SymSequence:
+class SymInstancePath:
 	"""
-	The sequence of execution nodes annotated with symbolic conditions required for killing a specific mutation
+	The sequence of symbolic instance being evaluated during testing
 	"""
 
-	def __init__(self, document: CDocument, target_mutant: jcmuta.Mutant):
+	def __init__(self, document: CDocument, line: str):
 		"""
-		:param document: 		the document that provides entire database
-		:param target_mutant: 	the mutation as target for being detected by tests
+		:param document:
+		:param line: mid [head condition+ ;]+
 		"""
 		self.document = document
-		self.mutant = target_mutant
-		self.executions = list()
-		self.words = list()
+		items = line.strip().split('\t')
+		self.mutant = self.document.project.muta_space.get_mutant(int(items[0].strip()))
+		self.instances = list()
+		self.conditions = set()
+		word_buffer = list()
+		for k in range(1, len(items)):
+			word = items[k].strip()
+			if len(word) > 0:
+				if word == ';':
+					head = word_buffer[0]
+					head_items = head.split('$')
+					exec_token = jcbase.CToken.parse(head_items[1].strip()).get_token_value()
+					executed = jcbase.CToken.parse(head_items[2].strip()).get_token_value()
+					accepted = jcbase.CToken.parse(head_items[3].strip()).get_token_value()
+					rejected = jcbase.CToken.parse(head_items[4].strip()).get_token_value()
+					execution = self.document.project.program.function_call_graph.get_execution(exec_token[0], exec_token[1])
+					instance = SymInstance(self, execution, executed, accepted, rejected)
+					self.instances.append(instance)
+					for j in range(1, len(word_buffer)):
+						sym_condition = self.document.conditions.get_condition(word_buffer[j].strip())
+						instance.conditions.append(sym_condition)
+						self.conditions.add(sym_condition)
+					word_buffer.clear()
+				else:
+					word_buffer.append(word)
 		return
 
 	def get_document(self):
 		"""
-		:return: the document that provides entire database
+		:return: the document where the path is created
 		"""
 		return self.document
 
 	def get_mutant(self):
 		"""
-		:return: the mutation as target for being detected by tests
+		:return: the mutation to be killed by the condition in path
 		"""
 		return self.mutant
 
-	def get_executions(self):
+	def get_instances(self):
 		"""
-		:return: the set of execution nodes annotated with symbolic conditions in the program
+		:return: the sequence of instances (with symbolic conditions) to define the killing process.
 		"""
-		return self.executions
-
-	def get_execution(self, k: int):
-		"""
-		:param k:
-		:return: the kth execution node annotated with symbolic conditions in the sequence
-		"""
-		execution = self.executions[k]
-		execution: SymExecution
-		return execution
-
-	def get_words(self):
-		"""
-		:return: the set of words encoding the symbolic conditions among the execution nodes in the sequence
-		"""
-		return self.words
+		return self.instances
 
 	def get_conditions(self):
-		return self.document.get_conditions_lib().get_conditions(self.words)
+		"""
+		:return: the set of symbolic conditions used to kill the target mutant in the killing path.
+		"""
+		return self.conditions
 
 
 if __name__ == "__main__":
 	root_path = "/home/dzt2/Development/Code/git/jcsa/JCMutest/result/features"
+	print_condition = False
 	for file_name in os.listdir(root_path):
 		print("Testing on", file_name)
 		c_directory = os.path.join(root_path, file_name)
 		c_document = CDocument(c_directory, file_name)
 		print(file_name, "loads", len(c_document.get_mutants()), "mutants used and",
-			  len(c_document.get_sequences()), "symbolic sequences annotated with",
+			  len(c_document.get_paths()), "symbolic instance paths annotated with",
 			  len(c_document.get_conditions_lib().get_all_conditions()), "conditions.")
-		for sequence in c_document.get_sequences():
-			sequence: SymSequence
-			mutant = sequence.get_mutant()
-			for condition in sequence.get_conditions():
-				print("\t{}\t{}\t{}\t{}\t{}\t{}".format(mutant.get_muta_id(),
-														condition.get_category(),
-														condition.get_operator(),
-														condition.get_execution(),
-														condition.get_location().get_cir_code(),
-														condition.get_parameter()))
+		for instance_path in c_document.get_paths():
+			mutant = instance_path.get_mutant()
+			print("\tPath: Killing mutant#{} using {} instances and {} conditions.".
+				  format(mutant.get_muta_id(), len(instance_path.get_instances()), len(instance_path.get_conditions())))
+			for condition in instance_path.get_conditions():
+				condition: SymCondition
+				if print_condition:
+					print("\t\t--> {}\t{}\t{}\t{}\t{}\t{}".format(mutant.get_muta_id(),
+																  condition.get_category(),
+																  condition.get_operator(),
+																  condition.get_execution(),
+																  condition.get_location().get_cir_code(),
+																  condition.get_parameter()))
 		print()
 
---
+++ PyMuta/com/jcsa/mine2/feature.py
@@ -0,0 +1,26 @@
+"""
+This file defines the data model of features directly used for mining algorithms.
+"""
+
+
+import os
+import com.jcsa.libs.base as jcbase
+import com.jcsa.libs.code as jccode
+import com.jcsa.libs.muta as jcmuta
+import com.jcsa.libs.test as jctest
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
