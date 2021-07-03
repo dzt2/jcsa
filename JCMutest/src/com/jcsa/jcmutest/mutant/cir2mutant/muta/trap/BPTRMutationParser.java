e6a459b76566b04caa61117ab175730202a59c52
dzt2 <dzt2@ubuntu>
2021-07-03 12:11:10 +0800

    1

---
+++ JCMutest/src/com/jcsa/jcmutest/mutant/cir2mutant/muta/trap/BPTRMutationParser.java
@@ -0,0 +1,37 @@
+package com.jcsa.jcmutest.mutant.cir2mutant.muta.trap;
+
+import java.util.Map;
+
+import com.jcsa.jcmutest.mutant.cir2mutant.base.SymCondition;
+import com.jcsa.jcmutest.mutant.cir2mutant.base.SymConditions;
+import com.jcsa.jcmutest.mutant.cir2mutant.muta.CirMutationParser;
+import com.jcsa.jcmutest.mutant.mutation.AstMutation;
+import com.jcsa.jcparse.lang.irlang.CirTree;
+import com.jcsa.jcparse.lang.irlang.expr.CirExpression;
+import com.jcsa.jcparse.lang.irlang.graph.CirExecution;
+import com.jcsa.jcparse.lang.irlang.stmt.CirStatement;
+
+public class BPTRMutationParser extends CirMutationParser {
+	
+	@Override
+	protected CirStatement get_location(CirTree cir_tree, AstMutation mutation) throws Exception {
+		return this.get_end_statement(cir_tree, mutation.get_location());
+	}
+	
+	@Override
+	protected void generate_infections(CirTree cir_tree, CirStatement statement,
+			AstMutation mutation, Map<SymCondition, SymCondition> infections) throws Exception {
+		CirExpression expression = this.get_cir_expression(cir_tree, mutation.get_location());
+		boolean value;
+		switch(mutation.get_operator()) {
+		case trap_on_true:	value = true;	break;
+		case trap_on_false:	value = false;	break;
+		default: throw new IllegalArgumentException("Invalid operator: " + mutation.get_operator());
+		}
+		CirExecution execution = SymConditions.execution_of(statement);
+		SymCondition constraint = SymConditions.eva_expr(execution, expression, value);
+		SymCondition init_error = SymConditions.trp_exec(execution);
+		infections.put(init_error, constraint);
+	}
+	
+}
