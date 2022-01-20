40c26808378a13a6ebfb8df8a135cafb455a9407
dzt2 <643729712@qq.com>
2022-01-20 11:39:56 +0800

    111

---
+++ JCMutest/src/com/jcsa/jcmutest/mutant/sta2mutant/util/CirStateSubsumer.java
@@ -0,0 +1,19 @@
+package com.jcsa.jcmutest.mutant.sta2mutant.util;
+
+/**
+ * It infers the direct subsumed abstract states by a given state any way.
+ * 
+ * @author yukimula
+ *
+ */
+public class CirStateSubsumer {
+	
+	/* singleton mode */ /** constructor **/ private CirStateSubsumer() { }
+	private static final CirStateSubsumer subsumer = new CirStateSubsumer();
+	
+	
+	
+	
+	
+	
+}
