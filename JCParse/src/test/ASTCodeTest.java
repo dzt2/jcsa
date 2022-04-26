c69ac7348ac1673806756a6a097c3b88a42ef9d6
dzt2 <643729712@qq.com>
2022-04-27 01:23:00 +0800

    111

--- JCParse/src/test/ASTCodeTest.java
+++ JCParse/src/test/ASTCodeTest.java
@@ -1,9 +1,18 @@
 package test;
 
 import java.io.File;
+import java.io.FileWriter;
 
 import com.jcsa.jcparse.lang.ClangStandard;
+import com.jcsa.jcparse.lang.astree.AstNode;
+import com.jcsa.jcparse.lang.irlang.CirNode;
+import com.jcsa.jcparse.lang.program.AstCirEdge;
+import com.jcsa.jcparse.lang.program.AstCirLink;
+import com.jcsa.jcparse.lang.program.AstCirNode;
 import com.jcsa.jcparse.lang.program.AstCirTree;
+import com.jcsa.jcparse.lang.program.types.AstCirLinkType;
+import com.jcsa.jcparse.lang.program.types.AstCirNodeType;
+import com.jcsa.jcparse.lang.program.types.AstCirParChild;
 
 public class ASTCodeTest {
 
@@ -26,6 +35,9 @@ public class ASTCodeTest {
 
 			File output2 = new File(postfx + file.getName());
 			write_code(source, output2);
+			
+			File output3 = new File(postfx + file.getName() + ".aci");
+			write_ast_cir_tree(source, output3);
 		}
 
 		File[] ofiles = new File(postfx).listFiles();
@@ -56,5 +68,65 @@ public class ASTCodeTest {
 		source_program.get_ast_tree().generate(false, target_file);
 		parse(target_file);
 	}
-
+	private static String strip_code(String code, int max_length) throws Exception {
+		StringBuilder buffer = new StringBuilder();
+		for(int k = 0; k < code.length(); k++) {
+			char ch = code.charAt(k);
+			if(Character.isWhitespace(ch)) {
+				ch = ' ';
+			}
+			buffer.append(ch);
+			if(buffer.length() > max_length) {
+				buffer.append("..."); break;
+			}
+		}
+		return buffer.toString();
+	}
+	private	static void write_node(FileWriter writer, AstCirNode tree_node) throws Exception {
+		int node_id = tree_node.get_node_id();
+		AstCirNodeType node_type = tree_node.get_node_type();
+		AstNode ast_source = tree_node.get_ast_source();
+		Object token = tree_node.get_token();
+		String token_code = "null";
+		if(token != null) {
+			token_code = strip_code(token.toString(), 96);
+		}
+		AstCirParChild edge = tree_node.get_child_type();
+		String ast_class = ast_source.getClass().getSimpleName();
+		ast_class = ast_class.substring(3, ast_class.length() - 4).strip();
+		String ast_code = strip_code(ast_source.generate_code(), 96);
+		int code_line = ast_source.get_location().line_of();
+		
+		writer.write("[" + node_id + "]");
+		writer.write("\t" + node_type);
+		writer.write("\t{" + token_code + "}");
+		writer.write("\t<" + edge + ">\n");
+		writer.write("\t" + ast_class);
+		writer.write("\t#" + code_line);
+		writer.write("\t\"" + ast_code + "\"\n");
+		writer.write("\tChild\t");
+		for(AstCirNode child : tree_node.get_children()) {
+			writer.write("\t" + child.get_node_id());
+		}
+		writer.write("\n");
+		for(AstCirLink link : tree_node.get_links()) {
+			AstCirLinkType cir_type = link.get_type();
+			CirNode cir_node = link.get_target();
+			String cir_class = cir_node.getClass().getSimpleName();
+			cir_class = cir_class.substring(3, cir_class.length() - 4);
+			String cir_code = strip_code(cir_node.generate_code(true), 96);
+			writer.write("\tlink\t" + cir_type + "\t\"" + cir_code + "\"\n");
+		}
+		for(AstCirEdge dep_edge : tree_node.get_ou_edges()) {
+			writer.write("\tedge\t" + dep_edge.get_type() + "\t" + dep_edge.get_target().get_node_id() + "\n");
+		}
+	}
+	private	static void write_ast_cir_tree(AstCirTree tree, File output) throws Exception {
+		FileWriter writer = new FileWriter(output);
+		for(AstCirNode tree_node : tree.get_tree_nodes()) {
+			write_node(writer, tree_node);
+		}
+		writer.close();
+	}
+	
 }
