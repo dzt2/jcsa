aa80b210016e8c86742cbddfd3868779cd799878
dzt2 <643729712@qq.com>
2022-04-28 23:29:48 +0800

    111

---
+++ JCMutest/src/com/jcsa/jcmutest/mutant/ctx2mutant/muta/oprt/CirArithSubMutationParser.java
@@ -0,0 +1,123 @@
+package com.jcsa.jcmutest.mutant.ctx2mutant.muta.oprt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jcsa.jcparse.lang.lexical.COperator;
+import com.jcsa.jcparse.lang.symbol.SymbolExpression;
+
+public class CirArithSubMutationParser extends CirOperatorMutationParser {
+
+	@Override
+	protected boolean to_assign() throws Exception {
+		/** (x -= y; x = y) :: (x != 2y) --> (x = y) **/
+		SymbolExpression roperand = this.sym_expression(COperator.arith_mul, 2, this.get_roperand());
+		SymbolExpression condition = this.sym_expression(COperator.not_equals, this.get_loperand(), roperand);
+		return this.parse_by_condition_and_operator(condition, COperator.assign);
+	}
+
+	@Override
+	protected boolean arith_add() throws Exception {
+		/** (x - y; x + y) :: (y != 0) --> (x + y) **/
+		SymbolExpression condition = this.sym_expression(COperator.not_equals, this.get_roperand(), 0);
+		return this.parse_by_condition_and_operator(condition, COperator.arith_add);
+	}
+
+	@Override
+	protected boolean arith_sub() throws Exception { return this.report_equivalences(); }
+
+	@Override
+	protected boolean arith_mul() throws Exception {
+		return this.parse_by_operator(COperator.arith_mul);
+	}
+
+	@Override
+	protected boolean arith_div() throws Exception {
+		return this.parse_by_operator(COperator.arith_div);
+	}
+
+	@Override
+	protected boolean arith_mod() throws Exception {
+		List<Object> conditions = new ArrayList<Object>();
+		conditions.add(this.sym_expression(COperator.smaller_tn, this.get_loperand(), this.get_roperand()));
+		conditions.add(this.sym_expression(COperator.greater_tn, this.get_loperand(), 
+				this.sym_expression(COperator.arith_mul, this.get_roperand(), 2)));
+		return this.parse_by_condition_and_operator(this.sym_conjunction(conditions), COperator.arith_mod);
+	}
+
+	@Override
+	protected boolean bitws_and() throws Exception {
+		return this.parse_by_operator(COperator.bit_and);
+	}
+
+	@Override
+	protected boolean bitws_ior() throws Exception {
+		SymbolExpression condition = this.
+				sym_expression(COperator.not_equals, this.get_roperand(), 0);
+		return this.parse_by_condition_and_operator(condition, COperator.bit_or);
+	}
+
+	@Override
+	protected boolean bitws_xor() throws Exception {
+		List<Object> conditions = new ArrayList<Object>();
+		conditions.add(this.sym_expression(COperator.not_equals, this.get_roperand(), 0));
+		conditions.add(this.sym_expression(COperator.not_equals, this.sym_expression(
+					COperator.bit_and, this.get_loperand(), this.get_roperand()), 0));
+		return this.parse_by_condition_and_operator(conditions, COperator.bit_xor);
+	}
+
+	@Override
+	protected boolean bitws_lsh() throws Exception {
+		SymbolExpression condition = this.
+				sym_expression(COperator.not_equals, this.get_roperand(), 0);
+		return this.parse_by_condition_and_operator(condition, COperator.left_shift);
+	}
+
+	@Override
+	protected boolean bitws_rsh() throws Exception {
+		SymbolExpression condition = this.
+				sym_expression(COperator.not_equals, this.get_roperand(), 0);
+		return this.parse_by_condition_and_operator(condition, COperator.righ_shift);
+	}
+
+	@Override
+	protected boolean logic_and() throws Exception {
+		return this.parse_by_operator(COperator.logic_and);
+	}
+
+	@Override
+	protected boolean logic_ior() throws Exception {
+		return this.parse_by_operator(COperator.logic_or);
+	}
+
+	@Override
+	protected boolean greater_tn() throws Exception {
+		return this.parse_by_operator(COperator.greater_tn);
+	}
+
+	@Override
+	protected boolean greater_eq() throws Exception {
+		return this.parse_by_operator(COperator.greater_eq);
+	}
+
+	@Override
+	protected boolean smaller_tn() throws Exception {
+		return this.parse_by_operator(COperator.smaller_tn);
+	}
+
+	@Override
+	protected boolean smaller_eq() throws Exception {
+		return this.parse_by_operator(COperator.smaller_eq);
+	}
+
+	@Override
+	protected boolean equal_with() throws Exception {
+		return this.parse_by_operator(COperator.equal_with);
+	}
+
+	@Override
+	protected boolean not_equals() throws Exception {
+		return this.parse_by_operator(COperator.not_equals);
+	}
+
+}
