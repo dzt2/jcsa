66557cc017ad85ba81fa3bdeee92718a19c91853
dzt2 <643729712@qq.com>
2022-04-29 17:25:49 +0800

    111

---
+++ JCMutest/src/com/jcsa/jcmutest/mutant/ctx2mutant/muta/oprt/CirArithMulMutationParser.java
@@ -0,0 +1,119 @@
+package com.jcsa.jcmutest.mutant.ctx2mutant.muta.oprt;
+
+public class CirArithMulMutationParser extends CirOperatorMutationParser {
+
+	@Override
+	protected boolean to_assign() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean arith_add() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean arith_sub() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean arith_mul() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean arith_div() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean arith_mod() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean bitws_and() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean bitws_ior() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean bitws_xor() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean bitws_lsh() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean bitws_rsh() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean logic_and() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean logic_ior() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean greater_tn() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean greater_eq() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean smaller_tn() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean smaller_eq() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean equal_with() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean not_equals() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
