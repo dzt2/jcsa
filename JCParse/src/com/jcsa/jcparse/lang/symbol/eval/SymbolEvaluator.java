dfe651bd3bfe3c7cb43f360b9db6e883ea9e5d6b
dzt2 <643729712@qq.com>
2022-05-04 23:01:35 +0800

    3

--- JCParse/src/com/jcsa/jcparse/lang/symbol/eval/SymbolEvaluator.java
+++ JCParse/src/com/jcsa/jcparse/lang/symbol/eval/SymbolEvaluator.java
@@ -229,8 +229,7 @@ public class SymbolEvaluator {
 		}
 		
 		/* 3. it saves the function-calling state in output-state */
-		this.set_state_value(expression, output); 
-		return output;
+		this.set_state_value(function, output); return output;
 	}
 	/**
 	 * @param expression
